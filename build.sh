35c1aaebd1c4fa1b1a680d273ecf47b5431ea1c4
HumanEntity <human_entity@icloud.com>
2023-05-21 10:18:54 +0200

    Updated build.sh script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ ! -d "config" ]; then
+	mkdir config
+fi
+
+rm -rf ./config/*
 cp -r ~/.config/nvim ./config/nvim
 cp -r ~/.config/alacritty ./config/alacritty
 cp -r ~/.config/tmux ./config/tmux
+
+cp -r ~/.scripts scripts
